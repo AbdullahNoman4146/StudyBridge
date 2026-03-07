03f4eae77c79b03e22a678542099bd21dce24f51
Zebra144 <sakybaky620@gmail.com>
2026-03-07 16:15:38 +0600

    Modified login and added student dashboard #9

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends Authenticatable implements JWTSubject
 {
@@ -11,12 +12,24 @@ class User extends Authenticatable implements JWTSubject
         'name',
         'email',
         'password',
-        'role_id'
+        'role',
+        'country_id',
+        'must_change_password',
+        'status'
     ];
 
     protected $hidden = [
         'password'
     ];
+
+    protected $casts = [
+        'must_change_password' => 'boolean',
+    ];
+
+    public function studentProfile(): HasOne
+    {
+        return $this->hasOne(StudentProfile::class, 'user_id');
+    }
 
     public function getJWTIdentifier()
     {
